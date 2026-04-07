817da4698db055827bcb93f384dff73bbaa486cf
angai0205 <a.ngai0205@gmail.com>
2026-04-07 13:15:41 -0400

    Fixed frontend fail

---
+++ frontend/src/components/PlayerViewPopUp.jsx
@@ -0,0 +1,4 @@
+import { Box, Container, Typography, IconButton } from "@mui/material";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
