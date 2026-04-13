eab3caabc003761ba6df7554e539328decadaa91
angai0205a <a.ngai0205@gmail.com>
2026-04-13 16:24:20 -0400

    Added API address in playerClient

--- frontend/src/api/playerClient.js
+++ frontend/src/api/playerClient.js
@@ -3,27 +3,22 @@ import axios from 'axios';
 const PLAYER_API_KEY = "admin_api_key"
 
 const playerClient = axios.create({
-    baseURL: process.env.REACT_APP_DO_YOU_KNOW_BALL,
-    headers: {
-        'X-API-Key': PLAYER_API_KEY
-    }
+  baseURL: process.env.REACT_APP_DO_YOU_KNOW_BALL ||
+    'https://do-you-know-ball-api.onrender.com',
+  headers: {
+    'X-API-Key': PLAYER_API_KEY
+  }
 });
 
 export const getAllPlayerNames = async () => {
-    const response = await playerClient.get('/players');
-    return response.data;
+  const response = await playerClient.get('/players');
+  return response.data;
 };
 
 export const getPlayerInfo = async (playerName) => {
-  const response = await axios.get(`
-    make a get req to https://do-you-know-ball-api.onrender.com/player`, 
-  {
-    params: {name: playerName },
-    headers: {
-        'X-API-Key': PLAYER_API_KEY
-    }
+  const response = await playerClient.get('/players', {
+    params: { name: playerName },
   });
-  
   return response.data
 }
 
