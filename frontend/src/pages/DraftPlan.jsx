0fbcc16f205f3f4fe2e2048128dbebc1dff9ae9e
angai0205 <a.ngai0205@gmail.com>
2026-05-13 21:05:20 -0400

    Forgot useMemo import in DraftPlan

--- frontend/src/pages/DraftPlan.jsx
+++ frontend/src/pages/DraftPlan.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState, useMemo } from "react";
 import { useParams } from "react-router-dom";
 import {
   Alert,
